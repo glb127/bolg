a9045200dec819d88b64bf4d94d9f8e96791fac7
guolb <guolb@udcredit.com>
2016-06-27 20:37:01 +0800

    change

--- bolg/cloud.js
+++ bolg/cloud.js
@@ -4,7 +4,11 @@ var AV = require('leanengine');
  * 一个简单的云代码方法
  */
 AV.Cloud.define('hello', function(request, response) {
-  response.success('Hello world!');
+	response.success('Hello world!');
+});
+
+AV.Cloud.define('retData', function(request, response) {
+	response.success('Data:'+JSON.stringify(request.params));
 });
 
 module.exports = AV.Cloud;
