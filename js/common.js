67d21fad234ed9a7b0fd1fef2234c5d744029e2b
guolb <guolb@udcredit.com>
2016-07-10 12:39:34 +0800

    key

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 (function(){
 	var _script = '<script src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>';
-	_script = '<script async src="https://testfp.udcredit.com/sdk/device-fingerprint/web?partnerCode=1000000000TEST&amp;appKey=9cWbNgUqiL91raHPVmrP"></script>';
+	_script = '<script async src="https://testfp.udcredit.com/sdk/device-fingerprint/web?partnerCode=201605170287&appKey=1buP1uYqnQiNi5fBM0GS"></script>';
 	//_script += '<script async src="/ydWallet/js/yongda_ud_fp.js"></script>';
 	//https://testfp.udcredit.com
 	//https://service.udcredit.com:10000
